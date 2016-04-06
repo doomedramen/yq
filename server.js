4c9b46e4ec5b90bf2caa5ef47248aaa01a7f2254
Martin Page <wookoouk@gmail.com>
2016-04-06 12:49:00 +0100

    test

--- server.js
+++ server.js
@@ -5,10 +5,28 @@ var slurm = require('./lib/slurm');
 app.listen(3000, function () {
   console.log('starting monitor');
 
+  var previous = [];
 
-  //setInterval(function () {
-  slurm.scontrol();
-  //}, 1000)
+  setInterval(function () {
+    var out = slurm.scontrol();
+
+    out.map(function (a) {
+      if (previous.filter(function (p) {
+          return p.JobId == a.JobId;
+        }).length < 1) {
+        console.log('new job', a.JobId);
+      }
+    });
+
+    previous.map(function (p) {
+      if (out.filter(function (o) {
+          return o.JobId == p.JobId;
+        }).length < 1) {
+        console.log('finished job', p.JobId);
+      }
+    })
+
+  }, 1000)
 
 
 });
