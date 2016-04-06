056d06cc69d5c9bf3a0f9bfe8378b3f80853fcfc
Martin Page <wookoouk@gmail.com>
2016-04-06 13:37:51 +0100

    test

--- server.js
+++ server.js
@@ -12,31 +12,24 @@ app.listen(3000, function () {
 
 
       out.map(function (o) {
-        if (previous.indexOf(o) < 0) {
+        var match = previous.filter(function (p) {
+          return p.JobId == o.JobId;
+        });
+
+        if (match.length < 1) {
           console.log('new job', o.JobId);
         }
       });
 
       previous.map(function (p) {
-        if (out.indexOf(p) < 0) {
-          console.log('finished job', p.JobId);
+        var match = out.filter(function (o) {
+          return p.JobId == o.JobId;
+        });
+
+        if (match.length < 1) {
+          console.log('finished jon', p.JobId);
         }
       });
-      //out.map(function (a) {
-      //  if (previous.filter(function (p) {
-      //      return p.JobId == a.JobId;
-      //    }).length < 1) {
-      //    console.log('new job', a.JobId);
-      //  }
-      //});
-
-      //previous.map(function (p) {
-      //  if (out.filter(function (o) {
-      //      return o.JobId == p.JobId;
-      //    }).length < 1) {
-      //    console.log('finished job', p.JobId);
-      //  }
-      //});
 
       previous = out;
     });
@@ -46,6 +39,6 @@ app.listen(3000, function () {
     reload();
   }, 10000);
 
-  reload() //run now too
+  reload(); //run now too
 
 });
